6221e2a1c794ddf47ad5b42c690aad973cf03ca6
BhushanK23 <kuranjekarb88@gmail.com>
2023-03-13 16:18:12 +0530

    terraform code added

---
+++ terraform.tfvars
@@ -0,0 +1,10 @@
+resource_group = "Bhushan-RG"
+location = "East US"
+azurerm_virtual_network = "ProdVnet"
+address_space = [ "10.0.0.0/16" ]
+azurerm_subnet = "internal"
+azurerm_network_interface = "Prod-NIC"
+azurerm_linux_virtual_machine = "ProdLinuxVM"
+size = [ "Standard_F2" ]
+
+
---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "resource_group" {
+    description = "this is bhushan RG"
+    type = string
+}
+variable "location" {
+    type = string
+  
+}
+variable "azurerm_virtual_network" {
+    description = "this is Prod Vnet"
+    type = string
+ }
+ variable "address_space" {
+    description = "Vnet address space for Pord"
+    type = list(string)
+   }
+variable "azurerm_subnet" {
+    description = "subnet space name"
+    type = string
+  
+}
+variable "address_prefixes" {
+  description = "Virtual Network CIDR range for all the environmetns except Dev"
+  type = list(string)
+  default = ["10.1.0.0/16"]
+  
+}
+variable "azurerm_network_interface" {
+    type = string
+  
+}
+variable "private_ip_address_allocation" {
+    type = string
+    default = "Dynamic"
+  
+}
+variable "azurerm_linux_virtual_machine" {
+    type = string
+  
+}
+variable "size" {
+    type = list(string)
+  
+}
